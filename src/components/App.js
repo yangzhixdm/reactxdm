7fc5f0e57844f83d961f9955e95a621ed75b070d
kevinyang <kevinyang@wezhuiyi.com>
2021-01-14 14:25:15 +0800

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,50 @@
-function App() {
-  return (
-    <div className="App">
+import React from 'react'
+import Info from './Info'
+import Header from './header'
+import axios from 'axios'
+
+class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'xx',
+    };
+  }
+
+  async handler () {
+    this.setState({
+      text: 'hello'
+    })
+
+    let data = await axios({
+      method: "get",
+      url: '/login',
+      data: {},
+      transformRequest: [
+        function (data) {
+          let ret = "";
+          for (let it in data) {
+            ret += encodeURIComponent(it) + "=" + encodeURIComponent(data[it]) + "&";
+          }
+          return ret;
+        }
+      ],
+      headers: {
+          "x-csrf-token": "yRkctP726mNIhsph2hyI4SuS"
+      }
+    })
+
+    console.log(data['data'])
+  }
+
+  render () {
+    return <div className="page">
+      <Header></Header>
+      <Info></Info>
+      { this.state.text }
+      <button onClick={ () => { this.handler() }}>Test</button>
     </div>
-  );
+  }
 }
 
-export default App;
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './static/index.css';
-import App from './components/Hello';
+import "./static/styles/reset.css"
+import './static/styles/index.css';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
